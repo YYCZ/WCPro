5ee8c34da8e9e8f67966284ec448d4788e0633ed
evilkant <137407874@qq.com>
2018-03-29 18:01:11 +0800

    优化了核心模块

--- yycz/Core.java
+++ yycz/Core.java
@@ -2,6 +2,8 @@ package yycz;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class Core {
 	
@@ -11,6 +13,7 @@ public class Core {
 		this.content=content;
 	}
 	
+	/*以朴素的办法统计词频,O(n^2)*/
 	private void updateWords(ArrayList<Word> words,String str) {
 		boolean newWord=true;
 		for(Word word : words) {
@@ -26,24 +29,49 @@ public class Core {
 		}
 	}
 	
+	/*以哈希的办法统计词频,O(n)*/
+	private void updateWordsWithHash(HashMap<String,Integer> map,String str) {
+		if(map.containsKey(str))
+			map.replace(str,map.get(str)+1);
+		else
+			map.put(str, 1);
+	}
+	
+	private void hashmapToWordList(HashMap<String,Integer> map,ArrayList<Word> words) {
+		if(map.isEmpty()) {
+			words=null;
+			return;
+		}
+		for(Entry<String, Integer> entry : map.entrySet()) {
+			Word word=new Word(entry.getKey(),entry.getValue());
+			words.add(word);
+		}
+	}
+	
 	public ArrayList<Word> countWords(){
 		ArrayList<Word> words=new ArrayList<Word>();
-		boolean inWord=false;
+		HashMap<String,Integer> map=new HashMap<String,Integer>();
 		char ch;
-		String str="";
+		String str;
+		boolean inWord=false;
+		StringBuilder sb=new StringBuilder();
 		content+=";";
 		for(int i=0;i<content.length();i++) {
 			ch=content.charAt(i);
 			if(Character.isLetter(ch)||ch=='-'&&Character.isLetter(content.charAt(i+1))) {
-				str+=ch;
+				sb.append(ch);
 				inWord=true;
 			}
 			else if(inWord) {
-				updateWords(words,str);
+				str=sb.toString();
+				str=str.toLowerCase();
+				//updateWords(words,str);
+				updateWordsWithHash(map, str);
 				inWord=false;
-				str="";
+				sb=new StringBuilder();
 			}
 		}
+		hashmapToWordList(map, words);
 		Collections.sort(words);
 		return words;
 	}
