2c8eb9bcace489977fe318e5dfa61eaf4e627caa
evilkant <137407874@qq.com>
2018-03-29 18:06:33 +0800

    优化了核心模块

--- yycz/Input.java
+++ yycz/Input.java
@@ -13,22 +13,27 @@ public class Input {//文件输入模块
 
 		if (args.length==0){
 			System.out.println("参数为空（请输入一个txt格式文件作为参数）");			
-			return false;
+			return true;
 		}
 		
 		if (args.length==1){
 			
+			File f = new File(args[0]);
 			String fileType = args[0].substring(args[0].lastIndexOf(".") + 1, args[0].length()).toLowerCase();
 			String fType="txt";
-			
-			if (fType.matches(fileType)){							
-				return true;				
-			}			
-			System.out.println("参数类型不匹配（仅处理txt格式文件）");
-			return false;		
-		}
+							
+			if (fType.matches(fileType)){
+				if(f.exists()) 
+					return false;
+				System.out.println("文件不存在！");
+				return true;
+		     }		
+					
+				System.out.println("参数类型不匹配（仅处理txt格式文件）");
+				return true;															
+		}				
 			System.out.println("参数个数不符合（限定为一个）");
-			return false;							
+			return true;							
 	}
 	
 	public String getContent() throws IOException {
--- yycz/Main.java
+++ yycz/Main.java
@@ -1,17 +1,18 @@
 package yycz;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		long startTime=System.currentTimeMillis();
 		
 		Input in=new Input(args);
 		
-		if(in.inValid()) {
-			System.out.println("");
+		if(in.inValid())
 			return;
-		}
 		
 		String content=in.getContent();
 		
@@ -19,10 +20,17 @@ public class Main {
 		
 		ArrayList<Word> words=core.countWords();
 		
-		Output out=new Output(words);
+		System.out.println(words.size());
 		
-		out.write();
+/*		for(int i=0;i<words.size();i++)
+			System.out.println(words.get(i).getStr()+" : "+words.get(i).getFrequency());
+*/		
+		long endTime=System.currentTimeMillis();
+		System.out.println("程序运行时间： " + (endTime-startTime) + "ms");
 		
+		//Output out=new Output(words);
+		
+		//out.write();
 	}
 
 }
