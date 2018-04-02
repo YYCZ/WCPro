a7f0b727792356787aa3c33b4998f1dc94fdcd91
lililili <tong9761@qq.com>
2018-04-02 20:37:11 +0800

    add output function

--- yycz/Output.java
+++ yycz/Output.java
@@ -1,9 +1,11 @@
 package yycz;
 
 import java.util.ArrayList;
+import java.io.*;
+import java.io.IOException;
 
 public class Output {
-	
+
 	private String filePath;
 	private ArrayList<Word> words;
 
@@ -11,8 +13,32 @@ public class Output {
 		this.filePath="result.txt";
 		this.words=words;
 	}
-	
-	public void write() {
-		
+
+	public void write() throws IOException {
+		String str="";
+		int flag =1;
+		for(Word word: words){
+			if(flag==1){
+				str+=word.getStr()+' '+word.getFrequency();
+				flag =0;
+			}
+			else str+="\r\n"+word.getStr()+' '+word.getFrequency();
+		}
+		System.out.println(str);
+		File afile =new File(filePath);
+		try {
+			afile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		BufferedWriter output = null;
+		try {
+			output = new BufferedWriter(new FileWriter(afile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		output.write(str);
+		output.flush();
+		output.close();
 	}
 }
